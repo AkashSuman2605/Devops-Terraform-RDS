5ed602d541fbd09510de5a5aabff900c6d5d2a9a
AKASH SUMAN <52396829+AkashSuman2605@users.noreply.github.com>
2026-07-07 16:38:37 +0530

    Create backend.tf

---
+++ infra/envs/dev/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+
+  backend "s3" {
+
+    bucket = "hotel-booking-terraform-state"
+
+    key = "dev/terraform.tfstate"
+
+    region = "ap-south-1"
+
+    dynamodb_table = "terraform-locks"
+
+    encrypt = true
+
+  }
+
+}
