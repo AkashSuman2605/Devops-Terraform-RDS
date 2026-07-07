c679a7c0fec6d5539be72d73c297477172ba076f
AKASH SUMAN <52396829+AkashSuman2605@users.noreply.github.com>
2026-07-07 13:31:52 +0530

    Create variables.tf

---
+++ infra/modules/network/variables.tf
@@ -0,0 +1,35 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_1_cidr" {
+  type = string
+}
+
+variable "public_subnet_2_cidr" {
+  type = string
+}
+
+variable "private_app_subnet_1_cidr" {
+  type = string
+}
+
+variable "private_app_subnet_2_cidr" {
+  type = string
+}
+
+variable "private_db_subnet_1_cidr" {
+  type = string
+}
+
+variable "private_db_subnet_2_cidr" {
+  type = string
+}
+
+variable "availability_zone_1" {
+  type = string
+}
+
+variable "availability_zone_2" {
+  type = string
+}
