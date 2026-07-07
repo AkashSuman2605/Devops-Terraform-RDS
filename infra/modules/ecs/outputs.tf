a8c16726f18a212e0626ec3e861d6c3f26bbddc3
AKASH SUMAN <52396829+AkashSuman2605@users.noreply.github.com>
2026-07-07 16:08:35 +0530

    Create outputs.tf

---
+++ infra/modules/ecs/outputs.tf
@@ -0,0 +1,11 @@
+output "alb_dns_name" {
+  value = aws_lb.alb.dns_name
+}
+
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.cluster.id
+}
+
+output "ecs_security_group_id" {
+  value = aws_security_group.ecs_sg.id
+}
