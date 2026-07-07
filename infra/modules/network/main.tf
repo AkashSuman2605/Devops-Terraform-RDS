f2b389d51c1886aa85fb3cdb3ad9dba46677f194
AKASH SUMAN <52396829+AkashSuman2605@users.noreply.github.com>
2026-07-07 14:55:37 +0530

    Update main.tf

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -5,3 +5,124 @@ resource "aws_vpc" "main" {
     Name = "main-vpc"
   }
 }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_1_cidr
+  availability_zone       = var.availability_zone_1
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_2_cidr
+  availability_zone       = var.availability_zone_2
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+resource "aws_subnet" "private_app_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_app_subnet_1_cidr
+  availability_zone = var.availability_zone_1
+
+  tags = {
+    Name = "private-app-subnet-1"
+  }
+}
+resource "aws_subnet" "private_app_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_app_subnet_2_cidr
+  availability_zone = var.availability_zone_2
+
+  tags = {
+    Name = "private-app-subnet-2"
+  }
+}
+resource "aws_subnet" "private_db_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_db_subnet_1_cidr
+  availability_zone = var.availability_zone_1
+
+  tags = {
+    Name = "private-db-subnet-1"
+  }
+}
+resource "aws_subnet" "private_db_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_db_subnet_2_cidr
+  availability_zone = var.availability_zone_2
+
+  tags = {
+    Name = "private-db-subnet-2"
+  }
+}
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "main-nat"
+  }
+
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private_app_1" {
+  subnet_id      = aws_subnet.private_app_subnet_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_app_2" {
+  subnet_id      = aws_subnet.private_app_subnet_2.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "private_db_1" {
+  subnet_id      = aws_subnet.private_db_subnet_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_db_2" {
+  subnet_id      = aws_subnet.private_db_subnet_2.id
+  route_table_id = aws_route_table.private.id
+}
