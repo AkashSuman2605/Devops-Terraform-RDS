fbc7d824f446401710ec82373210d7c9f32652a1
AKASH SUMAN <52396829+AkashSuman2605@users.noreply.github.com>
2026-07-07 16:26:35 +0530

    Update main.tf

--- infra/envs/prod/main.tf
+++ infra/envs/prod/main.tf
@@ -15,3 +15,47 @@ module "network" {
   availability_zone_1 = var.availability_zone_1
   availability_zone_2 = var.availability_zone_2
 }
+module "ecs" {
+
+  source = "../../modules/ecs"
+
+  vpc_id = module.network.vpc_id
+
+  public_subnet_ids = module.network.public_subnet_ids
+
+  private_subnet_ids = module.network.private_app_subnet_ids
+
+  container_image = "nginx:latest"
+
+  container_port = 80
+
+  desired_count = 2
+
+  execution_role_name = "ecsExecutionRole"
+
+  cluster_name = "hotel-cluster"
+
+}
+module "rds" {
+
+  source = "../../modules/rds"
+
+  vpc_id = module.network.vpc_id
+
+  private_db_subnet_ids = module.network.private_db_subnet_ids
+
+  ecs_security_group_id = module.ecs.ecs_security_group_id
+
+  db_name = var.db_name
+
+  db_username = var.db_username
+
+  db_password = var.db_password
+
+  db_instance_class = var.db_instance_class
+
+  backup_retention_period = var.backup_retention_period
+
+  deletion_protection = var.deletion_protection
+
+}
