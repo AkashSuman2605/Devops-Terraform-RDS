816790062f7bdb26523edfb0364b078a29b63bd8
AKASH SUMAN <52396829+AkashSuman2605@users.noreply.github.com>
2026-07-07 16:10:16 +0530

    Update main.tf

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -15,3 +15,24 @@ module "network" {
   availability_zone_1 = var.availability_zone_1
   availability_zone_2 = var.availability_zone_2
 }
+module "ecs" {
+
+  source = "../../modules/ecs"
+
+  vpc_id = module.network.vpc_id
+
+  public_subnet_ids = module.network.public_subnet_ids
+
+  private_subnet_ids = module.network.private_app_subnet_ids
+
+  container_image = "nginx:latest"
+
+  container_port = 80
+
+  desired_count = 1
+
+  execution_role_name = "ecsExecutionRole"
+
+  cluster_name = "hotel-cluster"
+
+}
