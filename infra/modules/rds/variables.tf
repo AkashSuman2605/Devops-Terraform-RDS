3199159e6de2a8e63bfb1b58a7e9f68905cbd7d0
AKASH SUMAN <52396829+AkashSuman2605@users.noreply.github.com>
2026-07-07 16:12:45 +0530

    Create variables.tf

---
+++ infra/modules/rds/variables.tf
@@ -0,0 +1,35 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_db_subnet_ids" {
+  type = list(string)
+}
+
+variable "ecs_security_group_id" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
+variable "db_instance_class" {
+  type = string
+}
+
+variable "backup_retention_period" {
+  type = number
+}
+
+variable "deletion_protection" {
+  type = bool
+}
