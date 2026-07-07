78146e93688ef9b385c6660f65214d97b429bc98
AKASH SUMAN <52396829+AkashSuman2605@users.noreply.github.com>
2026-07-07 23:54:24 +0530

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,7 +5,7 @@ TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 mkdir -p backups
 
 docker exec hotel-mysql \
-mysqldump -uroot -proot123 hoteldb \
+mysqldump -uroot -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} \
 > backups/backup_$TIMESTAMP.sql
 
 echo "Backup created successfully."
