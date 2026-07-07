88b93c4952ec70391542459452272c4f1d04eb07
AKASH SUMAN <52396829+AkashSuman2605@users.noreply.github.com>
2026-07-07 23:46:54 +0530

    Create restore.sh

---
+++ scripts/restore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILE=$1
+
+if [ -z "$FILE" ]; then
+    echo "Usage: ./restore.sh backup_file.sql"
+    exit 1
+fi
+
+docker exec -i hotel-mysql \
+mysql -uroot -proot123 hoteldb \
+< "$FILE"
+
+echo "Database restored successfully."
